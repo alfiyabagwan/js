6cc6f65e923b7ccfdeb0178c3ce5b434615f0875
alfiyabagwan <58721987+alfiyabagwan@users.noreply.github.com>
2025-07-20 11:38:49 +0000

    datatypes

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId=144553 //use const when values is not changed 
+let accountEmail="alfiya123@gmail.com" //use let variable to declare variable name 
+var accountPassword="12345"
+accountCity="jaipur" // it is safe language that if we not write any variable but also it is store a variable memory 
+// but it is not a good practice to write a variables .
+// accountId=2 // not allowed
+ accountEmail="alfi123@gmail.com"
+ accountPassword="123455"
+ accountCity="pune"
+/* prefer not to use ,because of issue in block scope and functional scope  */
+ 
+console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity])// this console.table is used to write all varable in tabular format 
---
+++ 02_DataTypes.js
@@ -0,0 +1,21 @@
+"use Strict";// treat all js code as newer version
+
+
+//alert("hello") //we are using nodejs not browser
+console.log(3+
+    3) ;// code should be not like this it should be redable like console.log(3+3)
+console.log("Alfiya");
+
+/* JavaScript Primitive datatype
+    1. Number : 2 to power 53 it is range only
+    2. bigint: if your number is big then we use bigint
+    3. String: "",''
+    4. boolean : true/false
+    5. null  :  standalone value and it is used as object in datatype 
+    // null is representation of empty value .null we defined as value we are intentionally telling that is empty 
+    6. undefined: when you not define any value ex let state;
+    7. symbol: unique value define// when we use react then we use symbol for writting componenets
+*/
+// object
+console.log(typeof undefined)// output is undefined
+console.log(typeof null) //output is object
